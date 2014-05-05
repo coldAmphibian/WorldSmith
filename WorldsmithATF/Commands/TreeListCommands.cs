b74e204241c31d7fb3d242e64e2facf128bdc016
RoyAwesome <kylegar@kylegar.net>
2014-05-05 01:31:18 -0700

    Added 'Open' command to the context menu.  Working on a Folder browser for the dota 2 directory setting

--- WorldsmithATF/Commands/TreeListCommands.cs
+++ WorldsmithATF/Commands/TreeListCommands.cs
@@ -48,7 +48,7 @@ namespace WorldsmithATF.Commands
             commandService.RegisterCommand(TreeListCommandsEnum.OpenEditor,
                 null,
                 null,
-                "Open Editor",
+                "Open",
                 "Open the file in an editor",
                 this);
 
@@ -91,10 +91,11 @@ namespace WorldsmithATF.Commands
 
             TextFile text = contextRegistry.ActiveContext.As<TextFile>();
             ProjectFile file = contextRegistry.ActiveContext.As<ProjectFile>();
+            
             if (file == null) return false;
             switch(e)
             {
-                //case TreeListCommandsEnum.OpenEditor: return true;
+                case TreeListCommandsEnum.OpenEditor: return contextRegistry.ActiveContext.As<ProjectFolder>() == null;
                 case TreeListCommandsEnum.OpenSource: return text != null;
                 case TreeListCommandsEnum.Delete: return !file.InGCF;
                 case TreeListCommandsEnum.Rename: return !file.InGCF;
@@ -107,11 +108,32 @@ namespace WorldsmithATF.Commands
             TreeListCommandsEnum e = (TreeListCommandsEnum)commandTag;
             switch(e)
             {
+                case TreeListCommandsEnum.OpenEditor: OpenEditor(); break;
                 case TreeListCommandsEnum.OpenSource: OpenSource(); break;
                 case TreeListCommandsEnum.Delete: DeleteFile(); break;
                 case TreeListCommandsEnum.Rename: RenameFile(); break;
             }          
 
+
+        }
+
+        private void OpenEditor()
+        {
+            KVFile kvFile = contextRegistry.ActiveContext.As<KVFile>();
+            if(kvFile != null)
+            {
+                //Open the file in a Key-Value editor
+
+                return;
+            }
+
+            TextFile textFile = contextRegistry.ActiveContext.As<TextFile>();
+            if(textFile != null)
+            {
+                //Open the file in a Text Editor
+
+                textEditor.OpenDocument(textFile);
+            }
 
         }
 
