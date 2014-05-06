b265670d5869ed095bf0b9b4e02adb6f823065c1
U-1337z-3\Derek <Derek@1337z-3.(none)>
2014-05-06 19:33:43 -0400

    Fix a validation issue on dota paths

--- WorldsmithATF/GlobalSettings.cs
+++ WorldsmithATF/GlobalSettings.cs
@@ -8,11 +8,18 @@ namespace WorldsmithATF
 {
     public static class GlobalSettings
     {
+        private static char[] removeatend = { System.IO.Path.DirectorySeparatorChar, System.IO.Path.AltDirectorySeparatorChar };
+        private static string dotadir;
         public static string DotaDirectory
         {
-            get;
-            set;
-        }
+            get {return dotadir;}
+            set {
+				// Strip off the path separator at the end if the user put it in with one
+			    // This fixes other parts of the code behaving oddly, as many windows forms
+			    // components don't properly handle doubled path separators
+			    dotadir = value.TrimEnd(removeatend);;
+            }
+		}
 
         public static string CurrentProjectDirectory
         {
